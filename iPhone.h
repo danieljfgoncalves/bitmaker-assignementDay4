1602968c2a018ade427ec4f898bcf409bf8f0716
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-09 16:43:33 -0400

    test

---
+++ iPhone.h
@@ -0,0 +1,31 @@
+//
+//  iphone.h
+//  bitmaker-assignmentDay4
+//
+//  Created by Daniel Goncalves on 2015-04-09.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iPhone : NSObject
+
+// Ch.01 Based on the example of the car, think of an object you use every day. Identify a class for that object and write five actions you do with that object.
+/*
+call
+text
+email
+post
+browse
+*/
+
+// Ch.02 Given the list in exercise 2, use the following syntax to rewrite your list in this format:
+
+-(void) calling;
+-(void) texting;
+-(void) emailing;
+-(void) posting;
+-(void) browsing;
+
+
+@end
---
+++ iPhone.m
@@ -0,0 +1,31 @@
+//
+//  iphone.m
+//  bitmaker-assignmentDay4
+//
+//  Created by Daniel Goncalves on 2015-04-09.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import "iPhone.h"
+
+@implementation iPhone
+
+-(void) calling {
+    NSLog(@"My iPhone is calling ");
+}
+
+-(void) texting {
+    NSLog(@"My iPhone is texting ");
+}
+
+-(void) emailing {
+    NSLog(@"My iPhone is emailing ");
+}
+-(void) posting {
+    NSLog(@"My iPhone is posting");
+}
+-(void) browsing {
+    NSLog(@"My iPhone is browsing");
+}
+
+@end
