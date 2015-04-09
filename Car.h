ba5b54c24b36d32b1afdbfd99e2750386001081d
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-09 14:19:05 -0400

    Interfaces

---
+++ Car.h
@@ -0,0 +1,20 @@
+//
+//  Car.h
+//  bitmaker-assignmentDay4
+//
+//  Created by Daniel Goncalves on 2015-04-09.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject {
+
+    // Protected instance variables (not recommend)
+}
+
+@property (copy) NSString *model;
+
+-(void)drive;
+
+@end
---
+++ Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  bitmaker-assignmentDay4
+//
+//  Created by Daniel Goncalves on 2015-04-09.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
