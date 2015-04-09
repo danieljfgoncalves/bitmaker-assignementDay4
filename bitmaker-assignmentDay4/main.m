440a40b56bcc3ddaf7dfc087f8267ea2ec85c09c
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-09 15:34:52 -0400

    Class-Level Initialization 2

--- bitmaker-assignmentDay4/main.m
+++ bitmaker-assignmentDay4/main.m
@@ -13,7 +13,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        [Car setDefaultModel:@"Nissan Versa"];
+        /* [Car setDefaultModel:@"Nissan Versa"];
         
         Car *toyota = [[Car alloc]init];
         
@@ -25,7 +25,14 @@ int main(int argc, const char * argv[]) {
         toyota.model = @"Toyota Camry";
         NSLog(@"Changed the car to a %@", toyota.model);
         
-        [toyota drive];
+        [toyota drive]; */
+        
+        // Instantiating objects
+        Car *nissan = [[Car alloc]init];
+        NSLog(@"Created a %@", [nissan model]);
+        
+        Car *chevy = [[Car alloc]initWithModel:@"Chevy Corvette"];
+        NSLog(@"Created a %@, too.", chevy.model);
         
     }
     return 0;
